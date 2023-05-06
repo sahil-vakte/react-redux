31ad137c89c8db5a7979857962962bba4c73a168
Sahil <sahilvaktepatil123@gmail.com>
2023-05-06 10:57:25 +0530

    changing the state of variable

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./Counter.css"
+import { useSelector,useDispatch } from "react-redux";
+import {incNumber,decNumber} from "../../actions/index"
 
 const Counter = () => {
+
+  const updatedState = useSelector((state)=>state.changeTheNumber)
+  const dispatch = useDispatch()
+
   return (
     <div className="counter-flex-div">
       <div className="counter-width-div">
         <div className="counter-main-div">
-          <button>+</button>
-          <button>-</button>
+          <button
+          onClick ={()=>dispatch(incNumber())}
+          >+</button>
+
+          <h3>{updatedState}</h3>
+
+          <button
+          onClick ={()=>dispatch(decNumber())}
+          >-</button>
         </div>
       </div>
     </div>
